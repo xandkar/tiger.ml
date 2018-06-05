d3558060276fd0cd604b984d7c30780a09b9b493
Siraaj Khandkar <siraaj@khandkar.net>
2018-06-05 12:12:46 -0400

    Report error count

--- compiler/src/exe/tiger_tests.ml
+++ compiler/src/exe/tiger_tests.ml
@@ -158,7 +158,11 @@ end = struct
             p_indent 2; p "out: %s" parsing_out; p_ln ();
     );
     p "%s" bar_end; p_ln ();
-    exit !error_count
+    let error_count = !error_count in
+    let clr = (if error_count = 0 then Green else Red) in
+    p "Errors: %s" (color clr (string_of_int error_count)); p_ln ();
+    p "%s" bar_end; p_ln ();
+    exit error_count
 end
 
 let test_cases_from_book =
