ce2f1040878bdbda39c3e0a554b6f0221497eff0
Siraaj Khandkar <siraaj@khandkar.net>
2018-09-15 16:15:49 -0400

    Disambiguate variant label

--- compiler/src/exe/tigert.ml
+++ compiler/src/exe/tigert.ml
@@ -1,5 +1,5 @@
 let () =
   let dir = ref "testcases" in
-  Arg.parse [("-dir", String (fun s -> dir := s), "")] (fun _ -> ()) "";
+  Arg.parse [("-dir", Arg.String (fun s -> dir := s), "")] (fun _ -> ()) "";
   let dir = !dir in
   Tiger.Test.run (Tiger.Test_cases.all ~dir)
