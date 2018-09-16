fca49f4f3908de86d5895b6a8708216fbb4dd529
Siraaj Khandkar <siraaj@khandkar.net>
2018-09-16 13:23:19 -0400

    Add CLI options to execute different test suites

--- compiler/src/exe/tigert.ml
+++ compiler/src/exe/tigert.ml
@@ -1,5 +1,27 @@
+type suite =
+  | All  of {dir : string}
+  | Book of {dir : string}
+  | Micro
+
 let () =
-  let dir = ref "testcases" in
-  Arg.parse [("-dir", Arg.String (fun s -> dir := s), "")] (fun _ -> ()) "";
-  let dir = !dir in
-  Tiger.Test.run (Tiger.Test_cases.all ~dir)
+  let suite = ref Micro in
+  let spec = ref [] in
+  Arg.parse_dynamic
+    spec
+    (function
+    | "micro" ->
+        spec := []
+    | "book" ->
+        spec := [("-dir", Arg.String (fun dir -> suite := Book {dir}), "")]
+    | "all" ->
+        spec := [("-dir", Arg.String (fun dir -> suite := All  {dir}), "")]
+    | _ -> ()
+    )
+    "";
+  let suite =
+    match !suite with
+    | All  {dir} -> Tiger.Test_cases.all  ~dir
+    | Book {dir} -> Tiger.Test_cases.book ~dir
+    | Micro      -> Tiger.Test_cases.micro
+  in
+  Tiger.Test.run suite
